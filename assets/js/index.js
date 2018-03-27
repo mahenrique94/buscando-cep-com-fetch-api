10bcdd254e22c9a555afa2e224fc4de5e82d4fda
Matheus Castiglioni <matheushcastiglioni@gmail.com>
2018-03-26 22:09:09 -0300

    Subindo projeto para Github

---
+++ assets/js/index.js
@@ -0,0 +1,26 @@
+function buscarCep(event, form) {
+    event.preventDefault();
+    const inputCep = form.cep;
+    if (inputCep) {
+        const cep = inputCep.value;
+        if (cep.length === 8) {
+            const URL = `http://ws.matheuscastiglioni.com.br/ws/cep/find/${cep}/json`;
+            fetch(URL)
+                .then(resposta => resposta.json())
+                .then(data => mostrarResposta(data))
+                .catch(erro => console.error(erro));
+        }
+    }
+}
+
+function mostrarResposta(cep) {
+    const mensagem = `
+        CEP: ${cep.cep},
+        Logradouro: ${cep.logradouro},
+        Complemento: ${cep.complemento},
+        Bairro: ${cep.bairro},
+        Cidade ${cep.cidade},
+        Estado: ${cep.estado}
+    `;
+    alert(mensagem);
+}
